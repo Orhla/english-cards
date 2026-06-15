4a780f0a5aee9dc189ccdd6c70820d13b18cfcd2
Vladimir Kromm <vkromm@gmail.com>
2026-06-15 15:25:57 +0200

    правильная типизация

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth"
-import Yandex from "next-auth/providers/yandex"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
 import { authConfig } from "@/auth.config"
@@ -15,7 +14,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             const dbUser = await prisma.user.findUnique({
                 where: { id: user.id },
                 select: { role: true }
-            })           
+            })
             if (dbUser) {
                 token.role = dbUser.role
                 token.id = user.id
@@ -24,18 +23,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
 
         async session({ session, token }) {
-            if (session.user && token) {
-                if (typeof token === "object" && token !== null && 
-                    "id" in token && typeof token.id === "string" &&
-                     "role" in token && typeof token.role === "string") {
-                    session.user = {
-                        ...session.user,
-                        id: token.id,
-                        role: token.role
-                    } as typeof session.user & { id: string; role: string }
-                }
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id ?? "",
+                    role: token.role ?? "user",
+                },
             }
-            return session
         }
     }
 })
